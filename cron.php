d092e699ebeab702a4798aaa2a001f7091703421
Robert Russo <rrusso@lsu.edu>
2026-02-26 21:06:35 +0000

    Updated cron to only run from cli

--- cron.php
+++ cron.php
@@ -1,6 +1,11 @@
 #!/usr/bin/env php
 <?php
 
+if (PHP_SAPI !== 'cli') {
+    http_response_code(403);
+    exit('CLI only.');
+}
+
 require_once __DIR__ . '/src/scraper.php';
 
 $scraper = new Scraper();
